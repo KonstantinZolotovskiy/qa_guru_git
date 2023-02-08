141a7b29fe5621b77229b7da8d5e004f0ea6e402
KonstantinZolotovskiy <121113759+KonstantinZolotovskiy@users.noreply.github.com>
2023-02-08 15:07:26 +0700

    Update Conflict.java

--- src/test/java/Conflict.java
+++ src/test/java/Conflict.java
@@ -8,7 +8,7 @@ public class Conflict {
 
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
+        open("https://www.mail.ru/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
